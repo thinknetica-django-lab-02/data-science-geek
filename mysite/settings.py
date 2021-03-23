cb169d4b32c0c868bbc1d896b7e99bf3b61db95f
user <user@info.bk>
2021-03-23 14:11:30 +0300

    change Flatpage with CKEditor

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'django.contrib.flatpages',
+    'ckeditor',
 
     'main.apps.MainConfig',
 ]
@@ -116,5 +117,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+BASE_STATIC_DIR = BASE_DIR / "static"
+
+STATICFILES_DIRS = [
+    BASE_STATIC_DIR,
+]
 
 SITE_ID = 2
